c4a9a6cf4f750eb6c8e4acd504ccf87f5a227cb8
lucidprojects <contact@lucidprojects.com>
2020-04-21 10:46:56 -0400

    sensor calibration

---
+++ calibration_sensor_array/calibration_sensor_array.ino
@@ -0,0 +1,252 @@
+/*
+  Modified Sensor Calibration
+  created 29 Oct 2008
+  By David A Mellis
+  modified 30 Aug 2011
+  By Tom Igoe
+  modified 20 April 2020
+  By Jake Sherwood
+
+  http://arduino.cc/en/Tutorial/Calibration
+
+  Modified to iterate through an array of sensors and calibrate each sensor.  Storing values in an array of mins/maxes.
+  Uses RGB LED to denote different calibration modes.
+  Starts in yellow. blue calibrating, toggle red switching to next sensor back to blue. solid red when calibration complete
+
+*/
+
+#include <CapacitiveSensor.h>
+
+int red = A3; //this sets the red led pin
+int green = A4 ; //this sets the green led pin
+int blue = A5 ; //this sets the green led pin
+
+int ledVal = 0;
+int newLed = 0;
+
+int pot = A0;
+
+//CAPACITIVE
+const int N_CAPBTNS = 4;  // set this to total # of sensors
+int capBtns[N_CAPBTNS];   // sensor vars in an array so we can iterate through in for loop
+
+int sensorValue[N_CAPBTNS]; // the sensor value
+int sensorMin[N_CAPBTNS];   // minimum sensor value
+int sensorMax[N_CAPBTNS];   // maximum sensor value
+int sensorValueRaw[N_CAPBTNS]; // raw maximum sensor value - for debuging in loop
+
+bool calComplete = false; // boolean to exit calibration while loop on completion
+
+//initialize senors
+CapacitiveSensor btnSensor0 = CapacitiveSensor(11, 12);
+CapacitiveSensor btnSensor1 = CapacitiveSensor(11, 10);
+CapacitiveSensor btnSensor2 = CapacitiveSensor(8, 9);
+CapacitiveSensor btnSensor3 = CapacitiveSensor(6, 7);
+
+void setup() {
+  Serial.begin(9600);
+
+  // Starts LED Yellow
+  analogWrite(green, 255);
+  digitalWrite(red, LOW);
+  digitalWrite(blue, HIGH);
+
+
+  // RGB LED pinModes
+  pinMode(red, OUTPUT);
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+
+  while (!Serial) {
+    ; // Wait for serial port to connect so we can see what is going on.
+  }
+
+  Serial.println("Serial started");
+
+
+  // take initial sensor readings - more consistent calibration readings
+  readSensors();
+
+  // run callibration
+  runSensorCalibration();
+}
+
+void loop() {
+
+  handleCapSensors();
+
+  if (ledVal > 220 ) {
+    digitalWrite(green, LOW);
+    digitalWrite(red, HIGH);
+    digitalWrite(blue, HIGH);
+  } else {
+    analogWrite(green, ledVal);
+    analogWrite(red, ledVal);
+    digitalWrite(blue, HIGH);
+  }
+
+}
+
+
+void readSensors() {
+
+  capBtns[0] =  btnSensor0.capacitiveSensor(30);
+  capBtns[1] =  btnSensor1.capacitiveSensor(30);
+  capBtns[2] =  btnSensor2.capacitiveSensor(30);
+  capBtns[3] =  btnSensor3.capacitiveSensor(30);
+}
+
+
+
+
+// handle cap values in loop()
+void handleCapSensors() {
+  // read the sensors:
+  readSensors();
+
+  for (int c = 0; c < N_CAPBTNS; c++) {
+
+    sensorValue[c] = capBtns[c];
+    sensorValueRaw[c] = capBtns[c];
+
+    // apply the calibration to the sensor reading
+    sensorValue[c] = map(sensorValue[c], sensorMin[c], sensorMax[c], 0, 255);
+
+    // in case the sensor value is outside the range seen during calibration // this is for LED
+    sensorValue[c] = constrain(sensorValue[c], 0, 255);
+
+    // set ledVals fade from yellow to green based on calibrated reading
+    setLedVal(c);
+
+    //debugging - show values when sensor > 400
+    if (capBtns[c] > 400) {
+
+      Serial.print("sensorValue[");
+      Serial.print(c);
+      Serial.print("] = ");
+      Serial.print(sensorValue[c]);
+      Serial.print("\t");
+
+      Serial.print("sensorValueRaw[");
+      Serial.print(c);
+      Serial.print("] = ");
+      Serial.print(sensorValueRaw[c]);
+      Serial.print("\t");
+
+      Serial.print("sensorMin[");
+      Serial.print(c);
+      Serial.print("] = ");
+      Serial.print(sensorMin[c]);
+      Serial.print("\t");
+
+      Serial.print("sensorMax[");
+      Serial.print(c);
+      Serial.print("] = ");
+      Serial.print(sensorMax[c]);
+      Serial.println("\t");
+
+    }
+  }
+}
+
+
+void runSensorCalibration(){
+    while (calComplete == false) {
+    // take sensor readings again - more consistent calibration readings
+    readSensors();
+
+    for (int c = 0; c < N_CAPBTNS; c++) { // iterate through sensors
+
+      sensorMin[c] = 1023;        // initial minimum sensor value
+      sensorMax[c] = 0;           // initial maximum sensor value
+
+      if (c < N_CAPBTNS) {  // do this to only go through the loop once per number of sensors
+
+        digitalWrite(green, HIGH);
+        digitalWrite(red, HIGH);
+        digitalWrite(blue, LOW);
+
+        int millisNow = millis();
+
+        while (millis() < (millisNow + 3000)) {  // 3000 = take calibration for 3 seconds
+          setMinMaxVals(c);
+        }
+
+        // print which sensor just calibrated and toggle red LED to indicate next reading
+        Serial.println(c);
+        delay(1000);
+        digitalWrite(green, HIGH);
+        digitalWrite(red, LOW);
+        digitalWrite(blue, HIGH);
+        delay(300);
+
+      }
+    }
+    calComplete = true;  // after iterating though # sensors
+
+  }
+
+  if (calComplete == true) {
+    Serial.print("end of the array with length = ");
+    Serial.println(N_CAPBTNS);
+    Serial.println("hello world");
+
+    // signal the end of the calibration period  - LED will turn GREEN for a second
+    digitalWrite(green, LOW);
+    digitalWrite(red, HIGH);
+    digitalWrite(blue, HIGH);
+    delay(1000);
+  }
+
+}
+
+
+
+// used in setup() for calibration
+void setMinMaxVals(int myVar) {
+
+  capBtns[0] =  btnSensor0.capacitiveSensor(30);
+  capBtns[1] =  btnSensor1.capacitiveSensor(30);
+  capBtns[2] =  btnSensor2.capacitiveSensor(30);
+  capBtns[3] =  btnSensor3.capacitiveSensor(30);
+
+  sensorValue[myVar] = capBtns[myVar];  // take sensor X reading
+
+  // record the maximum sensor value
+  if (sensorValue[myVar] > sensorMax[myVar]) {
+    sensorMax[myVar] = sensorValue[myVar];
+  }
+
+  // record the minimum sensor value
+  if (sensorValue[myVar] < sensorMin[myVar]) {
+    sensorMin[myVar] = sensorValue[myVar];
+  }
+
+  Serial.print("sensorValue[");
+  Serial.print(myVar);
+  Serial.print("] = ");
+  Serial.print(sensorValue[myVar]);
+  Serial.print("\t");
+
+  Serial.print("sensorMin[");
+  Serial.print(myVar);
+  Serial.print("] = ");
+  Serial.print(sensorMin[myVar]);
+  Serial.print("\t");
+
+  Serial.print("sensorMax[");
+  Serial.print(myVar);
+  Serial.print("] = ");
+  Serial.print(sensorMax[myVar]);
+  Serial.println("\t");
+
+}
+
+
+void setLedVal(int myLedVal) {
+
+  if (sensorValueRaw[myLedVal] > 400)    {
+    ledVal = sensorValue[myLedVal];
+  }
+
+}
