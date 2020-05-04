5cfcbdb7b53fcde23b9b0e888b20a114953cbf39
Noah Kernis <nkernis@gmail.com>
2020-05-04 12:50:12 -0400

    started refactor for final version

---
+++ midi_can_cap_refact_calibration_w_encoder_chords_refactor_could_this_name_be_longer_refactor/capacitive_touch.h
@@ -0,0 +1,542 @@
+#include <CapacitiveSensor.h>
+#include "encoder_mode_select.h"
+
+// CAPACITIVE
+
+// can calibration will run first - canCalibrationTime  eg first 10 seconds
+// purple LED and set capacitance of can
+// store capacitance of can just touching / holding it
+int canCap;						// sensor object
+int canCalibrationTime = 10000; // set calibration time for can
+int canCapMax;
+int canCapMin;
+
+//initialize sensors
+// add >= 300kΩ resistor between send pin (11) & receive pin (12)
+// sensor wire connected to receive pin
+// higher resistor more sensitve 1mΩ or > will make it almost too sensitve - capacitve readings just by hovering
+// https://youtu.be/jco-uU5ZgEU?t=225  (more about capacitive send and receive)
+
+// input to channel mapping
+// b1: 1c  |  s1: 5c
+// b2: 2c  |  s2: 6c   
+// b3: 3c  |  s3: 7c   
+// b4: 4c  |  s4: 8c
+
+CapacitiveSensor slideSensor1 = CapacitiveSensor(12, 11);
+CapacitiveSensor slideSensor2 = CapacitiveSensor(12, 10);
+CapacitiveSensor slideSensor3 = CapacitiveSensor(9, 8);
+CapacitiveSensor slideSensor4 = CapacitiveSensor(9, 7);
+
+CapacitiveSensor btnSensor1 = CapacitiveSensor(6, 5);
+CapacitiveSensor btnSensor2 = CapacitiveSensor(6, 4);
+CapacitiveSensor btnSensor3 = CapacitiveSensor(14, 3);
+CapacitiveSensor btnSensor4 = CapacitiveSensor(14, 2);
+
+//CAP btn vars
+int capBtnState[] = {LOW, LOW, LOW, LOW};
+const int N_CAPBTNS = 4;
+int capBtns[N_CAPBTNS];
+int lastCapBtnState[] = {LOW, LOW, LOW, LOW};
+
+//CAP slide vars
+int capSlideState[] = {LOW, LOW, LOW, LOW};
+const int N_CAPSLIDES = 4;
+int capSlides[N_CAPSLIDES];
+int lastCapSlideState[] = {LOW, LOW, LOW, LOW};
+int slideNoteVal[N_CAPSLIDES];
+int slideNote[N_CAPSLIDES];
+
+//CAP calibration
+int calibrationTime = 4000; // set calibration time per sensor
+bool calComplete = false;	// boolean to exit calibration while loop on completion
+
+// combined array for all sensor calibration - not exactly sure what to do here.  starting combined
+// take note of order index assignment in readSensors() so iteration in separate arrays works - e.g. capSlides need be set first
+const int N_ALL_CAPSENS = 8;   // set this to total # of sensors (butons & slides)
+int allCapSens[N_ALL_CAPSENS]; // sensor vars in an array so we can iterate through in for loop
+
+int sensorValue[N_ALL_CAPSENS];	   // the sensor value
+int sensorMin[N_ALL_CAPSENS];	   // minimum sensor value
+int sensorMax[N_ALL_CAPSENS];	   // maximum sensor value
+int sensorValueRaw[N_ALL_CAPSENS]; // raw maximum sensor value - for debuging in loop
+
+int sensorMin_length;
+int sensorMax_length;
+
+int sensorMinMIN, sensorMaxMAX; // min max values after calibration
+int sensorMinBuffer = 600;		// buffer to prevent sensors from reading too low or 0
+
+// SCALE
+
+// the intervals in a major and natural minor scale:
+int major[] = {2, 2, 1, 2, 2, 2, 1};
+int naturalMinor[] = {2, 1, 2, 2, 1, 2, 2};
+
+// an array to hold the final notes of the scale:
+int scale[8];
+
+// start with middle C:
+int tonic = pitchC4;
+
+// note to play:
+int scaleValue = tonic;
+
+//LED
+int red = 17;	//this sets the red led pin
+int green = 18; //this sets the green led pin
+int blue = 19;	//this sets the blue led pin
+
+int ledVal = 0;
+
+// CALIBRATION FUNCTIONS
+
+// read sensor values
+void readSensors()
+{
+	allCapSens[0] = slideSensor1.capacitiveSensor(30);
+	allCapSens[1] = slideSensor2.capacitiveSensor(30);
+	allCapSens[2] = slideSensor3.capacitiveSensor(30);
+	allCapSens[3] = slideSensor4.capacitiveSensor(30);
+	allCapSens[4] = btnSensor1.capacitiveSensor(30);
+	allCapSens[5] = btnSensor2.capacitiveSensor(30);
+	allCapSens[6] = btnSensor3.capacitiveSensor(30);
+	allCapSens[7] = btnSensor4.capacitiveSensor(30);
+}
+
+// used to set can capacitance. used in various thresholds throughout program
+void canCalibration()
+{
+	// purple LED to denote can calibration mode
+	analogWrite(red, 80);
+	analogWrite(green, 0);
+	analogWrite(blue, 80);
+
+	int canCapMillisNow = millis();
+
+	while (millis() < (canCapMillisNow + canCalibrationTime))
+	{
+		Serial.println("check can capacitance");
+
+		canCap = slideSensor1.capacitiveSensor(30);
+
+		for (int c = 0; c < 5; c++)
+		{
+			canCap = slideSensor1.capacitiveSensor(30);
+
+			// record the maximum sensor value
+			if (canCap > canCapMax)
+			{
+				canCapMax = canCap;
+			}
+
+			// record the minimum sensor value
+			if (canCap < canCapMin)
+			{
+				canCapMin = canCap;
+			}
+
+			if (canCap > 20)
+			{
+				Serial.print("canCap = ");
+				Serial.println(canCap);
+			}
+		}
+	}
+}
+
+void setLedVal(int myLedVal)
+{
+	if (sensorValueRaw[myLedVal] > 400)
+	{
+		ledVal = sensorValue[myLedVal];
+	}
+
+	if (ledVal > 220)
+	{
+		digitalWrite(green, LOW);
+		digitalWrite(red, HIGH);
+		digitalWrite(blue, HIGH);
+	}
+	else
+	{
+		analogWrite(green, ledVal);
+		analogWrite(red, ledVal);
+		digitalWrite(blue, HIGH);
+	}
+}
+
+// read/show cap values adjust led usd in loop()
+// this is mostly for debugging and can be removed / commented out in final code - js 20200421
+void capCalibration_Debug()
+{
+	// read the sensors:
+	readSensors();
+
+	for (int c = 0; c < N_ALL_CAPSENS; c++)
+	{
+		sensorValue[c] = allCapSens[c];
+		sensorValueRaw[c] = allCapSens[c];
+
+		// apply the calibration to the sensor reading
+		sensorValue[c] = map(sensorValue[c], sensorMin[c], sensorMax[c], 0, 255);
+
+		// in case the sensor value is outside the range seen during calibration // this is for LED
+		sensorValue[c] = constrain(sensorValue[c], 0, 255);
+
+		// set ledVals fade from yellow to green based on calibrated reading
+		setLedVal(c);
+
+		int capDebugVals;
+
+		if (canCapMax > 100)
+		{
+			capDebugVals = canCapMax + sensorMinBuffer;
+		}
+		else
+		{
+			capDebugVals = 400;
+		}
+
+		//debugging - show values when sensor > 400
+		if (allCapSens[c] > capDebugVals)
+		{
+			Serial.print("sensorValue[");
+			Serial.print(c);
+			Serial.print("] = ");
+			Serial.print(sensorValue[c]);
+			Serial.print("\t");
+
+			Serial.print("sensorValueRaw[");
+			Serial.print(c);
+			Serial.print("] = ");
+			Serial.print(sensorValueRaw[c]);
+			Serial.print("\t");
+
+			Serial.print("sensorMin[");
+			Serial.print(c);
+			Serial.print("] = ");
+			Serial.print(sensorMin[c]);
+			Serial.print("\t");
+
+			Serial.print("sensorMax[");
+			Serial.print(c);
+			Serial.print("] = ");
+			Serial.print(sensorMax[c]);
+			Serial.println("\t");
+		}
+	}
+}
+
+// get min and max values from sensorMin & sensorMax arrays
+int getMinMax(int *array, int size)
+{
+	int sensorMinMIN = array[0];
+	for (int i = 0; i < size; i++)
+	{
+		if (array[i] < sensorMinMIN)
+		{
+			sensorMinMIN = array[i];
+		}
+	}
+
+	int tempMinimum = sensorMinMIN;
+	for (int j = 0; j < size; j++)
+	{
+		if (array[j] > tempMinimum)
+		{
+			sensorMaxMAX = array[j];
+		}
+	}
+
+	// sets maximum in above for loop but returns minimum to prevent 0 value min
+	return sensorMinMIN;
+}
+
+// used in setup() for calibration
+void setMinMaxVals(int myVar)
+{
+	readSensors();
+
+	sensorValue[myVar] = allCapSens[myVar]; // take sensor X reading
+
+	// record the maximum sensor value
+	if (sensorValue[myVar] > sensorMax[myVar])
+	{
+		sensorMax[myVar] = sensorValue[myVar];
+	}
+
+	// record the minimum sensor value
+	if (sensorValue[myVar] < sensorMin[myVar])
+	{
+		sensorMin[myVar] = sensorValue[myVar];
+	}
+
+	Serial.print("sensorValue[");
+	Serial.print(myVar);
+	Serial.print("] = ");
+	Serial.print(sensorValue[myVar]);
+	Serial.print("\t");
+
+	Serial.print("sensorMin[");
+	Serial.print(myVar);
+	Serial.print("] = ");
+	Serial.print(sensorMin[myVar]);
+	Serial.print("\t");
+
+	Serial.print("sensorMax[");
+	Serial.print(myVar);
+	Serial.print("] = ");
+	Serial.print(sensorMax[myVar]);
+	Serial.println("\t");
+}
+
+// main calibration fn used in setup()
+void runSensorCalibration()
+{
+	while (calComplete == false)
+	{
+		// take sensor readings again - more consistent calibration readings
+		readSensors();
+
+		// iterate through sensors
+		for (int c = 0; c < N_ALL_CAPSENS; c++)
+		{
+			sensorMin[c] = 1023; // initial minimum sensor value
+			sensorMax[c] = 0;	 // initial maximum sensor value
+
+			// do this to only go through the loop once per number of sensors
+			if (c < N_ALL_CAPSENS)
+			{
+
+				digitalWrite(green, HIGH);
+				digitalWrite(red, HIGH);
+				digitalWrite(blue, LOW);
+
+				int millisNow = millis();
+
+				// 3000 = take calibration for 3 seconds - set in calibrationTime var
+				while (millis() < (millisNow + calibrationTime))
+				{
+					setMinMaxVals(c);
+				}
+
+				// print which sensor just calibrated and toggle red LED to indicate next reading
+				Serial.println(c);
+				delay(1000);
+				digitalWrite(green, HIGH);
+				digitalWrite(red, LOW);
+				digitalWrite(blue, HIGH);
+				delay(300);
+			}
+		}
+
+		// find min and max values from sensorMin & sensorMax arrays
+		sensorMin_length = sizeof(sensorMin) / sizeof(int);
+		sensorMax_length = sizeof(sensorMax) / sizeof(int);
+
+		int sensorMinMIN = getMinMax(sensorMin, sensorMin_length); // pass the array and its size
+		int sensorMinMAX = getMinMax(sensorMax, sensorMax_length); // pass the array and its size
+
+		Serial.println(sensorMinMIN);
+		Serial.println(sensorMaxMAX);
+
+		calComplete = true; // after iterating though # sensors
+	}
+
+	if (calComplete == true)
+	{
+		Serial.print("end of calibration array with length = ");
+		Serial.println(N_ALL_CAPSENS);
+		Serial.println("hello world");
+
+		// signal the end of the calibration period  - LED will turn GREEN for a second
+		digitalWrite(green, LOW);
+		digitalWrite(red, HIGH);
+		digitalWrite(blue, HIGH);
+		delay(1000);
+	}
+}
+
+void checkCapBtnVals(int myVar)
+{
+	// threshold value is set by canCapMax (set by canCalibration() fn) + sensorMinBuffer. can be adjusted to dial in sensitivity
+	if (capBtns[myVar] > canCapMax + sensorMinBuffer)
+	{
+		capBtnState[myVar] = HIGH;
+	}
+	else
+	{
+		capBtnState[myVar] = LOW;
+	}
+}
+
+void checkCapSlideVals(int myVar)
+{
+	// threshold value is set by canCapMax (set by canCalibration() fn) + sensorMinBuffer. can be adjusted to dial in sensitivity
+	if (capSlides[myVar] > canCapMax + sensorMinBuffer)
+	{
+		capSlideState[myVar] = HIGH;
+
+		// slide sensors need to always be first in the shared N_ALL_CAPSENS array
+		// so we can iterate properly in the N_CAPSLIDES array
+		// and properlly set min / max vals
+		slideNoteVal[myVar] = map(capSlides[myVar], sensorMin[myVar], sensorMax[myVar], 0, 90); // these values return from calibration fn
+	}
+	else
+	{
+		capSlideState[myVar] = LOW;
+	}
+}
+
+void handleCapSlides()
+{
+	capSlides[0] = slideSensor1.capacitiveSensor(30);
+	capSlides[1] = slideSensor2.capacitiveSensor(30);
+	capSlides[2] = slideSensor3.capacitiveSensor(30);
+	capSlides[3] = slideSensor4.capacitiveSensor(30);
+
+	for (int b = 0; b < N_CAPSLIDES; b++)
+	{
+		checkCapSlideVals(b);
+	}
+
+	for (int c = 0; c < N_CAPSLIDES; c++)
+	{
+
+		if (asSlides)
+		{
+			slideNote[c] = slideNoteVal[c];
+			slideNote[c] = constrain(slideNote[c], 10, 90);
+		}
+		else
+			slideNote[c] = baseNote;
+
+		int capSlideStateVal = capSlideState[c];
+
+		if (capSlideStateVal == HIGH)
+		{
+			Serial.println("turn midi slide signal on");
+
+			// all cap inputs play scale
+			if (doScale == true)
+			{
+				// midiOn(8 - c, scale[4+c]);
+				midiOn(9, scale[4 + c]); // send scale notes all on same channel
+				Serial.print("Sending scale note scale[");
+				Serial.print(c);
+				Serial.println("]");
+			}
+
+			// all cap inputs play scale
+			if (doScaleMod == true)
+			{
+				pitchBend = map(slideNoteVal[c], 10, 90, 0, 16383); //constrain values could be adjusted here
+				pitchBend = pitchBend << 1;							// shift low byte's msb to high byte
+				msb = highByte(pitchBend);							// get the high bits
+				lsb = lowByte(pitchBend) >> 1;						// get the low 8 bits
+
+				// print the values, for reference:
+				Serial.print("msb = ");
+				Serial.print(msb);
+				Serial.print(" lsb = ");
+				Serial.println(lsb);
+
+				// send the pitch bend message:
+				midiCommand(0xE8, lsb, msb, false); // pitchbend weirdness - first value is import here
+			}
+			else
+			{
+				midiOn(8 - c, slideNote[c]); // the first value sent in the fn will need to be adjusted for final channel assignment
+			}
+		}
+		else
+		{
+			// midiOff(8 - c, slideNote[c]);
+			// Serial.println("turn midi slide signal off");  // never turning it off helps slide effect
+			capSlideState[c] = LOW;
+		}
+
+		delay(10);
+	}
+}
+
+void handleCapBtns()
+{
+	capBtns[0] = btnSensor1.capacitiveSensor(30);
+	capBtns[1] = btnSensor2.capacitiveSensor(30);
+	capBtns[2] = btnSensor3.capacitiveSensor(30);
+	capBtns[3] = btnSensor4.capacitiveSensor(30);
+
+	for (int b = 0; b < N_CAPBTNS; b++)
+	{
+		checkCapBtnVals(b);
+	}
+
+	for (int c = 0; c < N_CAPBTNS; c++)
+	{
+
+		int capBtnStateVal = capBtnState[c];
+
+		if (capBtnStateVal != lastCapBtnState[c])
+		{
+			if (capBtnStateVal == HIGH)
+			{
+
+				// all cap inputs play scale
+				if (doScale == true || doScaleMod == true)
+				{
+					//midiOn(4 - c, scale[c]);
+					midiOn(9, scale[c]); // send scale notes all on same channel
+					Serial.print("Sending scale note scale[");
+					Serial.print(c);
+					Serial.println("]");
+					myScaleNote = scale[c];
+				}
+				else
+				{
+					midiOn(4 - c, baseNote); // the first value sent in the fn will need to be adjusted for final channel assignment
+				}
+			}
+			else
+			{
+				if (doScale == true || doScaleMod == true)
+				{
+					midiOff(9, baseNote);
+				}
+				else
+				{
+					midiOff(4 - c, baseNote); // the first value sent in the fn will need to be adjusted for final channel assignment
+				}
+
+				Serial.println("turn midi signal off");
+				capBtnState[c] = LOW;
+			}
+
+			lastCapBtnState[c] = capBtnStateVal;
+			delay(10);
+		}
+	}
+}
+
+void initCapacitive()
+{
+	// SCALE STUFF
+	// fill the scale array with the scale you want:
+	// start with the initial note:
+	scale[0] = tonic;
+	int note = scale[0];
+
+	// iterate over the intervals, adding each to the next note
+	// in the scale. You can change major to naturalMinor
+	// if you want that kind of scale instead:
+	for (int n = 0; n < 7; n++)
+	{
+		// note = note + naturalMinor[n];
+		note = note + major[n];
+		scale[n + 1] = note;
+	}
+
+	// run callibration
+	runSensorCalibration();
+}
---
+++ midi_can_cap_refact_calibration_w_encoder_chords_refactor_could_this_name_be_longer_refactor/encoder_mode_select.h
@@ -0,0 +1,321 @@
+#include <Encoder.h>
+#include "button_multipress.h"
+
+Encoder knob(A6, A7); // initialize encoder
+
+//const int buttonPin = 5;  // defining rotary button pin in button_multipress.h
+int lastKnobState = 0; // set last knob state
+int rtCounter = 0;	   //incremet rtCounter for accurate counts instead of encoder variance.
+int lastRtCounter = 0;
+
+int isPot = 0;
+int b;
+
+int channel;	  //store channel value
+int savedChannel; //preserve channel when changing octave
+int adsrVal = 0;  //ADSR value
+int lastAdsrVal;  // check if ADSR val changes
+int adsrType;
+
+/*ADSR type
+  1 ~ 49 (73 in hex) = Attack
+  2 ~ 4B (75 in hex) = Decay
+  3 ~ 40 (64 in hex)? = Sustain  (Hold Pedal)
+  4 ~ 48 (72 in hex) = Release
+*/
+int adsrTypeArray[4] = {73, 75, 64, 72};
+
+bool adsrTypeSet = false;
+bool selectCh = false;
+bool selectADSR = false;
+bool asSlides = true;
+bool doScale = false;
+bool doRecord = false;
+bool doBroadCast = false;
+bool doScaleMod = false;
+bool asChords = false;
+bool chordsOctave = false;
+bool firstOctaveChange = true;
+bool octaveChanged = false;
+
+// constrain encoder values
+void constrainEnc(int minE, int maxE)
+{
+	rtCounter = constrain(rtCounter, minE, maxE);
+}
+
+// eneable channel select in the loop
+void selectChannel()
+{
+	if (b == 1)
+	{
+		b = 0; //set b = 0 to prevent click entering mode
+	}
+
+	selectCh = true; // set mode bool to true for handling in loop
+}
+
+// eneable ADSR select in the loop
+void ADSR()
+{
+	if (b == 1)
+	{
+		b = 0; //set b = 0 to prevent click entering mode
+	}
+
+	selectADSR = true; // set mode bool to true for handling in loop
+}
+
+// use slides as slides or notes
+void slidesOrNotes()
+{
+	asSlides = !asSlides;
+	Serial.println(asSlides);
+}
+
+// use cap buttons a scales
+void capsAsScales()
+{
+	doScale = !doScale;
+	Serial.println(doScale);
+}
+
+void doRecordLoop()
+{
+	if (b == 1)
+	{
+		b = 0; //set b = 0 to prevent click entering mode
+	}
+
+	doRecord = true; // set mode bool to true for handling in loop
+	Serial.println(doRecord);
+}
+
+void broadCast()
+{
+	//forward midi cmd per channel to receiver.
+	//need to set channel to pass somehow.
+	if (b == 1)
+	{
+		b = 0; //set b = 0 to prevent click entering mode
+	}
+	doBroadCast = true; // set mode bool to true for handling in loop
+}
+
+void modScale()
+{
+	//  Mod scale - 4btns as scales sliders modify btns
+	doScaleMod = !doScaleMod; // toggle scale mod on and off
+	Serial.println(doScaleMod);
+}
+
+void modChords()
+{
+	//  Mod chords - 4btns as chordss sliders modify btns
+	asChords = true; // toggle scale mod on and off
+	//  Serial.println("asChords = ");
+	//  Serial.println(asChords);
+}
+
+void setMode(int myMode)
+{
+	/*
+   		NOTE: MODES
+		0) explore 
+			- different instruments all 8 channels
+		1) ch select
+		2) chords (use ch select)  
+			-  w pitch bend
+		3) scales (use ch select) (all 8 cap sensors) 
+			- TODO: need to figure out why slides play scale & explore???
+		4) scales (use ch select)  
+			- w pitch bend
+		5) slides as slides 
+			- defaults to slides (1)
+		6) pot mode
+		7) WARN: NOT DOING 
+			- volume adjust per channel - to play with things on and off
+		8) broadcast
+  */
+
+	Serial.print("set mode = ");
+	Serial.println(myMode);
+
+	switch (myMode)
+	{
+	case 0: // set default encoderMode
+		//OLED display.write("select mode");
+		isPot = 0;
+		selectCh = false;
+		doScaleMod = false;
+		asSlides = true;
+		doScale = false;
+		asChords = false;
+		break;
+	case 1: // channel select mode;
+		isPot = 3;
+		Serial.println("channel select mode");
+		//OLED display.write("select channel");
+		selectChannel();
+		break;
+	case 2: // Play chords - 4btns as chords sliders modify btns
+		Serial.println("chord mode w/ pitch bend");
+		//isPot = 6;
+		//OLED display.write(""Chord mode");
+		modChords();
+		break;
+	case 3: // All cap inputs as scale buttons mode
+		Serial.println("All cap inputs as scale buttons mode");
+		//OLED display.write("asCaps" + asCaps");
+		capsAsScales();
+		break;
+	case 4: // Mod scale - 4btns as scales sliders modify btns
+		Serial.println("Mod Scale mode");
+		isPot = 5;
+		//OLED display.write(""Mod Scale mode");
+		modScale();
+		break;
+	case 5: // Slides or notes mode
+		Serial.println("Slides or notes mode");
+		//OLED display.write("asSlides" + asSlides");
+		slidesOrNotes();
+		break;
+	case 6: // Explore mode - encoder as pot
+		Serial.println("Explore mode enc as pot");
+		isPot = 1;
+		//OLED display.write("Explore mode");
+		break;
+	case 7: // volume adjust per channel
+		Serial.println("volume adjust X CHANNEL");
+		isPot = 7;
+		//OLED display.write(""volume adjust channel" + channel);
+		// volumeAdjust();
+		break;
+	case 8: // Broadcast X CHANNEL
+		Serial.println("Broadcast X CHANNEL");
+		isPot = 3;
+		//OLED display.write("broadcast channel" + channel);
+		broadCast();
+		break;
+	default:
+		break;
+	}
+}
+
+void encoderButton(int b)
+{
+
+	switch (b)
+	{
+	case 0:
+		break;
+	case 1:
+		Serial.println("single click");
+
+		if (isPot == 0)
+		{
+			setMode(rtCounter);
+		}
+
+		break;
+	case 2:
+		Serial.println("double click");
+		setMode(0);
+		break;
+	case 3:
+		Serial.println("press and hold");
+		break;
+	case 4:
+		Serial.println("long press and hold");
+		break;
+	default:
+		setMode(rtCounter);
+		break;
+	}
+}
+
+int readEnc(int encMode)
+{
+	// Tom's encoder code
+	// checks for absolute value to check for forward or back movement
+	// incrementing rtCounter for better accuracy from the encoder
+	int knobState = knob.read();
+	int knobChange = knobState - lastKnobState;
+
+	// if it's changed by 4 or more pulses:
+	if (abs(knobChange) >= 4)
+	{
+		//want direction of change, not value, so
+		// divide value by abs. value to get 1 or -1:
+		knobChange = knobChange / abs(knobChange);
+
+		// if knobChange == 1, turned up/right rtCounter++, else rtCounter--
+		if (knobChange == 1)
+		{
+			rtCounter++;
+		}
+		else
+		{
+			rtCounter--;
+		}
+
+		// save the current state for comparison next time:
+		lastKnobState = knobState;
+
+		// handle whether encoder acts as encoder or "potentiometer" and the range
+		switch (encMode)
+		{
+		case 0: //default - mode select encoder
+			if (rtCounter < 1)
+			{
+				rtCounter = 8;
+			}
+
+			if (rtCounter > 8)
+			{
+				rtCounter = 1;
+			}
+
+			break;
+		case 1:	// encoder as "pot" for note exploration
+			constrainEnc(10, 110); // MIDI audible note values
+			break;
+		case 2:	// encoder for ADSR
+			constrainEnc(1, 127); // MIDI ADSR values
+			break;
+		case 3:	// encoder for channel select in ADSR & broadcast funcs
+			constrainEnc(1, 9); // # of MIDI channels
+			break;
+		case 4:	// encoder for ADSR type select
+			constrainEnc(1, 4); // (1 = attack, 2 = decay, 3 = sustain, 4 = release)
+			break;
+		case 5:	// encoder for pitchbend
+			constrainEnc(0, 1); // on/off for pitchbend
+			break;
+		case 6:	//encoder for chord octave switching
+			constrainEnc(0, 6); // n * 12 to change 7 octaves
+			break;
+		case 7:	//encoder for volume adjustment
+			constrainEnc(1, 127); //  changes volume 1-127
+			break;
+		case 8:	//encoder for channels 0 - 8 (1-9)
+			constrainEnc(0, 9); //   channels 0 - 8 (1-9)
+			break;
+		default:
+			break;
+		}
+		// Serial.print("rtCounter from readEnc() = ");
+		
+		// if (asChords == true && chordsOctave == false)
+		// {
+		// 	Serial.println(rtCounter);
+		// }
+		// else
+		// {
+		// 	Serial.println(rtCounter);
+		// }
+
+		Serial.println(rtCounter);
+		//OLED display.write("mode" + rtCounter);
+	}
+}
