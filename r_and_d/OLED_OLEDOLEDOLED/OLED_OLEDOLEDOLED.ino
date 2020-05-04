8d618bf0c0c556157e2b9f55be2aa0019fe9b48f
Noah Kernis <nkernis@gmail.com>
2020-05-04 11:44:27 -0400

    save

--- r_and_d/OLED_OLEDOLEDOLED/OLED_OLEDOLEDOLED.ino
+++ r_and_d/OLED_OLEDOLEDOLED/OLED_OLEDOLEDOLED.ino
@@ -38,12 +38,13 @@ void setup()
 	if (!display.begin(SSD1306_SWITCHCAPVCC, 0x3C))
 	{ // Address 0x3C for 128x32
 		Serial.println(F("SSD1306 allocation failed"));
-		for (;;)
-			; // Don't proceed, loop forever
+		for (;;); // Don't proceed, loop forever
 	}
 
 	// Clear the buffer
 	display.clearDisplay();
+	
+	display.setRotation(1);
 
 	// Draw a single pixel in white
 	display.drawPixel(10, 10, SSD1306_WHITE);
@@ -56,31 +57,7 @@ void setup()
 
 void loop()
 {
-	testdrawchar(); // Draw characters of the default font
 	testdrawstyles(); // Draw 'stylized' characters
-}
-
-void testdrawchar(void)
-{
-	display.clearDisplay();
-
-	display.setTextSize(1);				 // Normal 1:1 pixel scale
-	display.setTextColor(SSD1306_WHITE); // Draw white text
-	display.setCursor(0, 0);			 // Start at top-left corner
-	display.cp437(true);				 // Use full 256 char 'Code Page 437' font
-
-	// Not all the characters will fit on the display. This is normal.
-	// Library will draw what it can and the rest will be clipped.
-	for (int16_t i = 0; i < 256; i++)
-	{
-		if (i == '\n')
-			display.write(' ');
-		else
-			display.write(i);
-	}
-
-	display.display();
-	delay(2000);
 }
 
 void testdrawstyles(void)
@@ -90,15 +67,14 @@ void testdrawstyles(void)
 	display.setTextSize(1);				 // Normal 1:1 pixel scale
 	display.setTextColor(SSD1306_WHITE); // Draw white text
 	display.setCursor(0, 0);			 // Start at top-left corner
-	display.println(F("Hello, world!"));
+	display.println(F("M: 1"));
+	display.println();
+	display.println(F("Ch: 4"));
+	display.println();
+	display.println(F("A: 1"));
+	display.println();
 
-	display.setTextColor(SSD1306_BLACK, SSD1306_WHITE); // Draw 'inverse' text
-	display.println(3.141592);
-
-	display.setTextSize(2); // Draw 2X-scale text
-	display.setTextColor(SSD1306_WHITE);
-	display.print(F("0x"));
-	display.println(0xDEADBEEF, HEX);
+	// display.setTextSize(2); // Draw 2X-scale text
 
 	display.display();
 	delay(2000);
